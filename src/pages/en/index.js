5b63e2cce6a7aeb846c4e1b84df21da1dfcab429
Alexey Ovsyannikov <soliterco@gmail.com>
2023-02-08 20:26:54 +0300

    changed index files

--- src/pages/en/index.js
+++
@@ -1,9 +0,0 @@
-import BlogPage from "./BlogPage"
-import HomePage from "./HomePage"
-import ErrorPage from "./ErrorPage"
-import PrivacyPage from "./PrivacyPage"
-
-import BecameFrontDevPage from "./articles_pages/BecameFrontDevPage"
-import LucaDaCostaBookPage from "./articles_pages/LucaDaCostaBookPage"
-
-export { BlogPage, HomePage, ErrorPage, PrivacyPage, BecameFrontDevPage, LucaDaCostaBookPage};
